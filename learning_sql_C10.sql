1deeab618935cd7d2cbf72348c704dbd7cd5e69b
dkisai <diego.kisai@live.com>
2022-09-05 13:53:46 -0500

    Chapter10 archive

---
+++ learning_sql_C10.sql
@@ -0,0 +1,28 @@
+/*EXERCISE 10-1
+Using the following table definitions and data, write a query that returns each customer
+name along with their total payments:
+Customer:
+Customer_id Name
+----------- ---------------
+1 John Smith
+2 Kathy Jones
+3 Greg Oliver
+Payment:
+Payment_id Customer_id Amount
+---------- ----------- --------
+101 1 8.99
+102 3 4.99
+103 1 7.99
+Include all customers, even if no payment records exist for that customer.*/
+
+
+/*EXERCISE 10-2
+Reformulate your query from Exercise 10-1 to use the other outer join type (e.g., if
+you used a left outer join in Exercise 10-1, use a right outer join this time) such that
+the results are identical to Exercise 10-1.*/
+
+
+/*EXERCISE 10-3 (Extra Credit)
+Devise a query that will generate the set {1, 2, 3, ..., 99, 100}. (Hint: use a cross join
+with at least two from clause subqueries.)*/
+
