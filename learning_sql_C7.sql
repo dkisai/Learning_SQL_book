689da5de978b5e4078c438169bf1dc503d640201
dkisai <diego.kisai@live.com>
2022-09-05 13:44:27 -0500

    Chapter7 archive

---
+++ learning_sql_C7.sql
@@ -0,0 +1,13 @@
+/*EXERCISE 7-1
+Write a query that returns the 17th through 25th characters of the string 'Please
+find the substring in this string'.*/
+
+
+/*EXERCISE 7-2
+Write a query that returns the absolute value and sign (−1, 0, or 1) of the number
+−25.76823. Also return the number rounded to the nearest hundredth.*/
+
+
+/*EXERCISE 7-3
+Write a query to return just the month portion of the current date.*/
+
