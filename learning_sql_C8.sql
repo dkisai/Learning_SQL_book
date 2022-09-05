ebecce812009078dd6fe463a554f8e67449e9c6c
dkisai <diego.kisai@live.com>
2022-09-05 13:51:21 -0500

    Chapter8 archive

---
+++ learning_sql_C8.sql
@@ -0,0 +1,13 @@
+/*EXERCISE 8-1
+Construct a query that counts the number of rows in the payment table.*/
+
+
+/*EXERCISE 8-2
+Modify your query from Exercise 8-1 to count the number of payments made by each
+customer. Show the customer ID and the total amount paid for each customer.*/
+
+
+/*EXERCISE 8-3
+Modify your query from Exercise 8-2 to include only those customers who have
+made at least 40 payments.*/
+
