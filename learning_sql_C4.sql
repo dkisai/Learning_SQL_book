6728455767b780574826e9dc1fb1a4be51a2d4c6
dkisai <diego.kisai@live.com>
2022-09-05 13:30:16 -0500

    Chapter4 archive

---
+++ learning_sql_C4.sql
@@ -0,0 +1,19 @@
+/*EXERCISE 4-1
+Which of the payment IDs would be returned by the following filter conditions?
+customer_id <> 5 AND (amount > 8 OR date(payment_date) = '2005-08-23')*/
+
+
+/*EXERCISE 4-2
+Which of the payment IDs would be returned by the following filter conditions?
+customer_id = 5 AND NOT (amount > 6 OR date(payment_date) = '2005-06-19')*/
+
+
+/*EXERCISE 4-3
+Construct a query that retrieves all rows from the payments table where the amount
+is either 1.98, 7.98, or 9.98.*/
+
+
+/*EXERCISE 4-4
+Construct a query that finds all customers whose last name contains an A in the second
+position and a W anywhere after the A.*/
+
