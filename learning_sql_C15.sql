b2fb834371e08b564a9fef45bdb9f4ec61b95357
dkisai <diego.kisai@live.com>
2022-09-05 14:05:22 -0500

    Chapter15 archive

---
+++ learning_sql_C15.sql
@@ -0,0 +1,10 @@
+/*EXERCISE 15-1
+Write a query that lists all of the indexes in the Sakila schema. Include the table
+names.*/
+
+
+/*EXERCISE 15-2
+Write a query that generates output that can be used to create all of the indexes on the
+sakila.customer table. Output should be of the form:
+"ALTER TABLE <table_name> ADD INDEX <index_name> (<column_list>)"*/
+
