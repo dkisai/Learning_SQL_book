dc00207b2b3893d9d3f024a6b7772ab7a10ef680
dkisai <diego.kisai@live.com>
2022-12-09 18:04:08 -0600

    Chapter5 complete

--- learning_sql_C5.sql
+++ learning_sql_C5.sql
@@ -23,13 +23,32 @@ mysql> SELECT c.first_name, c.last_name, a.address, ct.city
 +------------+-----------+------------------------+----------------+
 9 rows in set (0.00 sec)*/
 
+SELECT c.first_name, c.last_name, a.address, ct.city
+FROM customer c
+INNER JOIN address a 
+ON c.address_id = a.address_id
+INNER JOIN city ct
+ON a.city_id = ct.city_id
+WHERE a.district = 'California';
+
 
 /*EXERCISE 5-2
 Write a query that returns the title of every film in which an actor with the first name
 JOHN appeared.*/
 
+SELECT title
+FROM film f
+INNER JOIN film_actor fa
+ON f.film_id = fa.film_id
+INNER JOIN actor a
+ON fa.actor_id = a.actor_id
+WHERE a.first_name = 'JOHN';
 
 /*EXERCISE 5-3
 Construct a query that returns all addresses that are in the same city. You will need to
 join the address table to itself, and each row should include two different addresses.*/
-
+SELECT a.address, aa.address, a.city_id
+FROM address a
+INNER JOIN address aa
+ON a.city_id = aa.city_id
+AND a.address < aa.address;
